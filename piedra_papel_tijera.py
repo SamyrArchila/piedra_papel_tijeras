52433c9796b3b697144d0722a892cae885444dfb
SamyrArchila <samirarchila@colegioguanenta.edu.co>
2025-02-12 07:57:21 -0500

    a

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,38 +1,43 @@
 import random
 
 print("----------------------")
-print("1)piedra")
-print("2)papel")
-print("3)tijera")
+print("Piedra,papel o tijera")
+print("----------------------")
+print("1. piedra")
+print("2. papel")
+print("3. tijera")
 print("----------------------")
 
-usu = int(input("Ingrese el numero correspondiente a su decision"))
-maq = random.randint(1,3)
+usuario = int(input("Ingrese el numero correspondiente a su decision: "))
+maquina = random.randint(1,3)
 
-if usu >= 4:
+if usuario not in [1, 2, 3]:
     print("No valido")
 else:
-    if usu == 1:
+    if usuario == 1:
         print("Elegiste: Piedra")
-    elif usu == 2:
+    elif usuario == 2:
         print("Elegiste: papel")
-    elif usu == 3:
+    elif usuario == 3:
         print("Elegiste: tijera")
 
-    if maq == 1:
+    if maquina == 1:
         print("La maquina eligio: Piedra")
-    elif maq == 2:
+    elif maquina == 2:
         print("La maquina eligio: papel")
-    elif maq == 3:
+    elif maquina == 3:
         print("La maquina eligio: tijera")
 
-    if usu == maq:
+    if usuario == maquina:
         print("Empate")
-    elif usu == 2 maq:
+    elif usuario == 1 and maquina == 1:
         print("Ganaste")
-    elif usu == 3 maq:
+    elif usuario == 2 and maquina == 2:
         print("Ganaste")
-    elif usu == 4 maq: 
+    elif usuario == 3 and maquina == 3:
+
+    
+
 
 
 
